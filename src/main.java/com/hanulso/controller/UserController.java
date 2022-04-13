1bd389fe7844fa0a7adcc7c838b75d9df9a49262
Jeong GH <cd_asat@hotmail.com>
2022-04-13 23:25:19 +0900

    아이디 중복 체크(경고창) 추가

--- src/main.java/com/hanulso/controller/UserController.java
+++ src/main.java/com/hanulso/controller/UserController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -413,7 +414,12 @@ public class UserController {
 	}
 	
 
-	
+	// 회원가입시 아이디 중복 체크
+	@PostMapping("/username_check.do")
+	@ResponseBody
+	public int userNameCheck(String username) {
+		return service.user_check(username);
+	}
 	
 	
 }
